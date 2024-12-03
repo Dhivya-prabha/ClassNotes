d059e632e47692c3a9a2d80822b1209176e45f89
Dhivya-prabha <49254873+Dhivya-prabha@users.noreply.github.com>
2024-12-03 19:18:53 +0530

    Update ReportExtent.java

--- ReportExtent.java
+++ ReportExtent.java
@@ -6,8 +6,11 @@ import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -42,27 +45,36 @@ public class ReportExtent {
 	        test = extent.createTest("Google Search Test", "This is a test for searching in Google");
 
 	        driver.get("https://www.google.com");
+	        driver.manage().window().maximize();
+	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	        File image = driver.getScreenshotAs(OutputType.FILE);
 	        
 	        FileUtils.copyFile(image, new File("./img1.png"));
 	        String title = driver.getTitle();
+	        
+	        
+	        driver.findElement(By.xpath("//textarea[@class='gLFyf']")).sendKeys("Java", Keys.ENTER);
+	        FileUtils.copyFile(image, new File("./img2.png"));
 	        test.assignAuthor("Dhivya Prabha");
 	        test.assignCategory("Smoke Test");
 
 	        // Log test steps and results
 	        if (title.equals("Google")) {
 	            test.pass("Navigated to Google successfully", MediaEntityBuilder.createScreenCaptureFromPath("./img1.png").build());
-	        } else {
-	        	
-	        	
-	        	
+	        } else {       	
+	        		        	
 	            test.fail("Navigation to Google failed");
 	        }
+	        test.pass("Navigated to Google successfully", MediaEntityBuilder.createScreenCaptureFromPath("./img2.png").build());
+	        
+	        test.fail("Navigation to Google failed",  MediaEntityBuilder.createScreenCaptureFromPath("./img3.png").build());
 	    }
+	    
+	    
 
 	    @AfterClass
 	    public void teardown() {
-	        driver.quit();
+	        //driver.quit();
 	        extent.flush();  // Write all logs to the report
 	    }
 	}
